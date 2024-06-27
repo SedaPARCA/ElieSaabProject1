35457041a8734e08296628ae978d110f0b5ee638
seda <sedaparca.93@gmail.com>
2024-06-27 15:54:33 +0300

    build denemesidir.

--- src/test/java/Runner/SmokeTest.java
+++ src/test/java/Runner/SmokeTest.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest",
         features = {"src/test/java/FeatureFiles"},
         glue={"StepDefinitions"},
-        plugin = {"json:target/cucumber/cucumber.json" } //sss
+        plugin = {"json:target/cucumber/cucumber.json" } //ssss
 
 )
 
