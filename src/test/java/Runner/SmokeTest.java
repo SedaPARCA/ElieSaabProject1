e5f7a1a48b3fab989611fb9648610efbf2398804
seda <sedaparca.93@gmail.com>
2024-06-27 15:34:08 +0300

    Login test işlemleri yapıldı...

--- src/test/java/Runner/SmokeTest.java
+++ src/test/java/Runner/SmokeTest.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest",
         features = {"src/test/java/FeatureFiles"},
         glue={"StepDefinitions"},
-        plugin = {"json:target/cucumber/cucumber.json" } //s
+        plugin = {"json:target/cucumber/cucumber.json" } //ss
 
 )
 
